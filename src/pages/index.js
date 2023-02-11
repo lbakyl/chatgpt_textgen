d96d7bb00daa19ef52b051f9a57c68e1dbda68c7
Jan Bakalar <jan.bachelor@gmail.com>
2023-02-11 14:26:18 +0100

    Fixed syntax (class -> className, strokelinecap -> strokeLinecap, etc. as per errors reported in the console.

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,26 +66,26 @@ const handleCancel = () =>{
         </p>        
       </header>
       {/*
-      <div class="md:hidden p-4 space-y-2 bg-slate-800 rounded shadow my-1">
-        <span class="block w-8 h-0.5 bg-gray-100 "></span>
-        <span class="block w-8 h-0.5 bg-gray-100 "></span>
-        <span class="block w-8 h-0.5 bg-gray-100 "></span>
+      <div className="md:hidden p-4 space-y-2 bg-slate-800 rounded shadow my-1">
+        <span className="block w-8 h-0.5 bg-gray-100 "></span>
+        <span className="block w-8 h-0.5 bg-gray-100 "></span>
+        <span className="block w-8 h-0.5 bg-gray-100 "></span>
       </div>
         */}
 
-<div class="flex w-full items-center bg-slate-100 px-4 py-2">
+<div className="flex w-full items-center bg-slate-100 px-4 py-2">
 
   <div className="md:hidden cursor-pointer" onClick={() => setIsNavOpen((prev) => !prev)}>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokewidth="2">
-      <path strokelinecap="round" strokelinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
     </svg>
   </div>
 </div>
 
 <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
 <div id="mobilemenu" className="flex flex-col bg-slate-50 p-8 justify-center items-start gap-2 md:hidden">
-  <div><button onClick={handleClickLoveLetter} type="button" class="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Love Letter</button></div>                
-  <div><button onClick={handleClickJobAdvert} type="button" class="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Job Advert</button></div>
+  <div><button onClick={handleClickLoveLetter} type="button" className="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Love Letter</button></div>                
+  <div><button onClick={handleClickJobAdvert} type="button" className="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Job Advert</button></div>
 </div>
 </div>
 
@@ -100,12 +100,12 @@ const handleCancel = () =>{
 
 
       <div className="bg-white my-1 w-full flex flex-col md:flex-row md:space-x-1 md:space-y-0">
-        <aside class="bg-green-300 md:w-1/3 lg:w-1/4 text-center py-10 hidden md:block space-y-3">
-          <h2 class="text-2xl md:text-2xl font-bold">Topics To Generate</h2>
-          <div><button onClick={handleClickLoveLetter} type="button" class="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Love Letter</button></div>                
+        <aside className="bg-green-300 md:w-1/3 lg:w-1/4 text-center py-10 hidden md:block space-y-3">
+          <h2 className="text-2xl md:text-2xl font-bold">Topics To Generate</h2>
+          <div><button onClick={handleClickLoveLetter} type="button" className="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Love Letter</button></div>                
           
           <div className="btn-list">
-            <button onClick={handleModalOpen} type="button" class="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Job advert</button>
+            <button onClick={handleModalOpen} type="button" className="text-black bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900">Job advert</button>
             <BtnModal visible={modal2Visible} onCancel={handleCancel} onOk={handleClickJobAdvert}/>
           </div>
           
@@ -116,7 +116,7 @@ const handleCancel = () =>{
      
         </aside>
 
-        <div class="bg-sky-500 md:w-3/3 lg:w-3/4 px-3 py-0 flex">
+        <div className="bg-sky-500 md:w-3/3 lg:w-3/4 px-3 py-0 flex">
           { IsJobAdvertSelected && <Job_Advert/> }
         </div>
 
@@ -125,8 +125,8 @@ const handleCancel = () =>{
   
 
 
-    <footer class="bg-slate-800 mt-auto p-5">
-        <h1 class="text-2xl md:text-4xl text-white">Footer</h1>
+    <footer className="bg-slate-800 mt-auto p-5">
+        <h1 className="text-2xl md:text-4xl text-white">Footer</h1>
     </footer>
 
 
